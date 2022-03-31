38cbbcffd50e3a91846005effdf446c287e40aef
olosoa <razakarivony@et.esiea.fr>
2022-03-31 22:49:43 +0200

    fonctionnalite reaction a larrivee dun utilisateur sur le channel general

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import discord
 
-client = discord.Client()
+default_intents=discord.Intents.default() #permissions par defaut
+default_intents.members = True #perissions relative aux membres 
+
+client = discord.Client(intents=default_intents)
 
 
+#evenement permettant d'afficher en cli que le bot est connecte sur discord
 @client.event
 async def on_ready():
 	print("Le bot est pret.")
 
 
+
+
+
+
+#evenement qui permet de gerer l'arrive de nouveaux membres 
+@client.event
+async def on_member_join(member):
+	general_channel: discord.TextChannel = client.get_channel(958694311342522430)
+	await  general_channel.send(content=f"Bienvenue sur le serveur {member.display_name} !")
+
+
+
+
+
+
+#evenement qui renvoie des messages du bot 
 @client.event
 async def on_message(message):  
 	#print(message.content) #affiche les messages entrer dans discord dans la console
@@ -19,4 +39,4 @@ async def on_message(message):
 
 
 
-client.run("OTU4NjkwODE0NTA5MzUxMDEy.YkRAZQ.YdcjZAmcwCTPVZUWzywEPQ5yZ30")
+client.run("OTU4NjkwODE0NTA5MzUxMDEy.YkRAZQ.YdcjZAmcwCTPVZUWzywEPQ5yZ30") 
